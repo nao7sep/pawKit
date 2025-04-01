0e3fd441464d68da5608312f26e1790da933f5b5
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-04-01 11:21:17 +0900

    avalonia added

---
+++ src/pawKit.Samples.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace pawKit.Samples.Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
