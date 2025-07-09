855a31ac9adca79b0178efe2a29203697242b288
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-07-09 10:43:51 +0900

    feat: add IRandomProvider interface and CryptoRandomProvider implementation for cryptographically secure random data generation

---
+++ src/pawKitLib/Abstractions/IRandomProvider.cs
@@ -0,0 +1,82 @@
+﻿namespace pawKitLib.Abstractions;
+
+/// <summary>
+/// Defines a contract for a provider of cryptographically secure random data,
+/// serving as a testable and injectable equivalent to <see cref="System.Random"/>.
+/// </summary>
+/// <remarks>
+/// This abstraction allows for dependency injection and testability of components
+/// that rely on random data generation. The default implementation should use a
+/// cryptographically secure random number generator.
+/// </remarks>
+public interface IRandomProvider
+{
+    /// <summary>
+    /// Fills a byte array with a cryptographically strong random sequence of values.
+    /// </summary>
+    /// <param name="byteCount">The number of random bytes to generate.</param>
+    /// <returns>An array containing the random bytes.</returns>
+    byte[] GetBytes(int byteCount);
+
+    /// <summary>
+    /// Fills the elements of a specified span of bytes with a cryptographically strong random sequence of values.
+    /// </summary>
+    /// <param name="buffer">The span to fill with random bytes.</param>
+    void GetBytes(Span<byte> buffer);
+
+    /// <summary>
+    /// Generates a cryptographically secure random integer that is within a specified range.
+    /// </summary>
+    /// <param name="fromInclusive">The inclusive lower bound of the random number returned.</param>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned. Must be greater than or equal to <paramref name="fromInclusive"/>.</param>
+    /// <returns>A random integer that is greater than or equal to <paramref name="fromInclusive"/> and less than <paramref name="toExclusive"/>.</returns>
+    int GetInt32(int fromInclusive, int toExclusive);
+
+    /// <summary>
+    /// Generates a non-negative cryptographically secure random integer that is less than the specified maximum.
+    /// </summary>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned. Must be greater than or equal to 0.</param>
+    /// <returns>A random integer that is greater than or equal to 0 and less than <paramref name="toExclusive"/>.</returns>
+    int GetInt32(int toExclusive);
+
+    /// <summary>
+    /// Generates a non-negative cryptographically secure random integer.
+    /// </summary>
+    /// <returns>A random integer that is greater than or equal to 0 and less than <see cref="int.MaxValue"/>.</returns>
+    /// <remarks>This method behaves identically to <see cref="System.Random.Next()"/>, producing a value in the range [0, int.MaxValue - 1].</remarks>
+    int GetInt32();
+
+    /// <summary>
+    /// Generates a cryptographically secure random 64-bit integer that is within a specified range.
+    /// </summary>
+    /// <param name="fromInclusive">The inclusive lower bound of the random number returned.</param>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned. Must be greater than or equal to <paramref name="fromInclusive"/>.</param>
+    /// <returns>A random 64-bit integer that is greater than or equal to <paramref name="fromInclusive"/> and less than <paramref name="toExclusive"/>.</returns>
+    long GetInt64(long fromInclusive, long toExclusive);
+
+    /// <summary>
+    /// Generates a non-negative cryptographically secure random 64-bit integer that is less than the specified maximum.
+    /// </summary>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned. Must be greater than or equal to 0.</param>
+    /// <returns>A random 64-bit integer that is greater than or equal to 0 and less than <paramref name="toExclusive"/>.</returns>
+    long GetInt64(long toExclusive);
+
+    /// <summary>
+    /// Generates a non-negative cryptographically secure random 64-bit integer.
+    /// </summary>
+    /// <returns>A random 64-bit integer that is greater than or equal to 0 and less than <see cref="long.MaxValue"/>.</returns>
+    /// <remarks>This method behaves identically to <see cref="System.Random.NextInt64()"/>, producing a value in the range [0, long.MaxValue - 1].</remarks>
+    long GetInt64();
+
+    /// <summary>
+    /// Returns a random floating-point number that is greater than or equal to 0.0 and less than 1.0.
+    /// </summary>
+    /// <returns>A double-precision floating point number.</returns>
+    double GetDouble();
+
+    /// <summary>
+    /// Returns a random floating-point number that is greater than or equal to 0.0 and less than 1.0.
+    /// </summary>
+    /// <returns>A single-precision floating point number.</returns>
+    float GetSingle();
+}
---
+++ src/pawKitLib/Security/CryptoRandomProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using pawKitLib.Abstractions;
+
+namespace pawKitLib.Security;
+
+/// <summary>
+/// A default implementation of <see cref="IRandomProvider"/> that uses the
+/// built-in <see cref="RandomNumberGenerator"/> for cryptographic-quality randomness.
+/// </summary>
+public sealed class CryptoRandomProvider : IRandomProvider
+{
+    /// <inheritdoc />
+    public byte[] GetBytes(int byteCount) =>
+        RandomNumberGenerator.GetBytes(byteCount);
+
+    /// <inheritdoc />
+    public void GetBytes(Span<byte> buffer) =>
+        RandomNumberGenerator.Fill(buffer);
+
+    /// <inheritdoc />
+    public int GetInt32(int fromInclusive, int toExclusive) =>
+        RandomNumberGenerator.GetInt32(fromInclusive, toExclusive);
+
+    /// <inheritdoc />
+    public int GetInt32(int toExclusive) =>
+        RandomNumberGenerator.GetInt32(toExclusive);
+
+    /// <inheritdoc />
+    public int GetInt32() =>
+        RandomNumberGenerator.GetInt32(int.MaxValue);
+
+    /// <inheritdoc />
+    public long GetInt64(long fromInclusive, long toExclusive) =>
+        RandomNumberGenerator.GetInt64(fromInclusive, toExclusive);
+
+    /// <inheritdoc />
+    public long GetInt64(long toExclusive) =>
+        RandomNumberGenerator.GetInt64(toExclusive);
+
+    /// <inheritdoc />
+    public long GetInt64() =>
+        RandomNumberGenerator.GetInt64(long.MaxValue);
+
+    /// <inheritdoc />
+    public double GetDouble()
+    {
+        // To get a value in the range [0.0, 1.0), we generate a random long in the range
+        // [0, long.MaxValue - 1] and then divide by long.MaxValue. This ensures the result
+        // is always strictly less than 1.0.
+        long randomLong = GetInt64(long.MaxValue);
+        return (double)randomLong / long.MaxValue;
+    }
+
+    /// <inheritdoc />
+    public float GetSingle()
+    {
+        // To get a value in the range [0.0f, 1.0f), we use a similar logic to GetDouble()
+        // but with single-precision floating-point numbers in mind. We generate a random
+        // integer in the range [0, int.MaxValue - 1] and divide by int.MaxValue.
+        // This prevents rounding errors that could result in 1.0f if we were to cast from a double.
+        int randomInt = GetInt32(int.MaxValue);
+        return (float)randomInt / int.MaxValue;
+    }
+}
